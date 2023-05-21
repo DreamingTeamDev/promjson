f3688c87e9e594d17e0ba9967c1e19ca92b927d8
efirnet <efirnet@gmail.com>
2023-05-21 18:16:56 +0300

    add rrd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+// import { Link } from "react-router-dom";
 import ProductList from "./components/ProductList";
 
 function App() {
   return (
     <div>
+      {/* <Link
+        to="./data/products.json"
+        style={{
+          width: "80%",
+          border: "1px solid black",
+          borderRadius: 10,
+          backgroundColor: "green",
+          color: "white",
+          padding: 10,
+          textDecoration: "none",
+        }}
+      >
+        JSON-смотреть
+      </Link> */}
       <ProductList />
     </div>
   );
